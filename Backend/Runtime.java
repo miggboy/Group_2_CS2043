05ec59b313a54384bae479756cbdb8284fab4f38
MaxMacNeill-UNB <d3n2c@unb.ca>
2023-11-08 16:18:01 -0400

    Implemented a very basic runtime
    I'll do the rest of the functionality later, I'm off to have dinner.

---
+++ Backend/Runtime.java
@@ -0,0 +1,27 @@
+package Backend;
+
+import java.util.ArrayList;
+
+public class Runtime {
+    private ArrayList<Ingredient> ingredientList;
+    private ArrayList<Recipe> recipeList;
+
+    public Runtime() {
+        try {
+        ingredientList = Ingredient.loadSavedList();
+        recipeList = Recipe.loadSavedList();
+        }
+        catch (Exception e){
+            System.out.println("The backend failed to initialize due to the following error: \n\n" + e.getMessage() + "\n\nA stack trace will follow.");
+            e.printStackTrace();
+        }
+    }
+
+    public ArrayList<Ingredient> getIngredients() {
+        return new ArrayList<Ingredient>(ingredientList);
+    }
+
+        public ArrayList<Recipe> getRecipes() {
+        return new ArrayList<Recipe>(recipeList);
+    }
+}
