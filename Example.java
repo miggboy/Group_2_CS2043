7cbd620be7dd95d486ba882518e7b74b4f973baf
MaxMacNeill-UNB <d3n2c@unb.ca>
2023-11-09 11:45:08 -0400

    Update Example.java
    Added a simple script to remove existing configuration before running the test case, allowing existing configuration files to be regenerated for debugging purposes - this should not be carried over into the frontend implementation.

--- Example.java
+++ Example.java
@@ -1,9 +1,20 @@
+import Backend.Ingredient;
 import Backend.Recipe;
 import Backend.Runtime;
+import java.io.File;
 
 public class Example {
 
   public static void main(String[] args) {
+    //Remove the existing configuration for debugging purposes.
+    File f = new File(System.getProperty("user.dir") + "\\.RecipeBrowser");
+    String[] entries = f.list();
+    for (String s : entries) {
+      File currentFile = new File(f.getPath(), s);
+      currentFile.delete();
+    }
+    f.delete();
+
     Runtime testing = new Runtime();
     Recipe rec = testing.getRecipes(0);
     int ingredientCount = rec.getIngredientCount();
