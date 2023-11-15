755eb4662abed0023cbd2aceb189980cea9d722a
miguel g <56207281+miggboy@users.noreply.github.com>
2023-11-15 14:01:39 -0400

    Add files via upload

---
+++ Frontend/Main.java
@@ -0,0 +1,40 @@
+package Frontend;
+	
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			Parent root = FXMLLoader.load(getClass().getResource("titleScreen.fxml"));
+			
+			Scene scene = new Scene(root);
+			scene.getStylesheets().add(getClass().getResource("CSS/application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			
+			primaryStage.setResizable(false);
+	        //primaryStage.initStyle(StageStyle.UNDECORATED);
+			URL url = getClass().getResource("IMG/icon.png");
+	        String str = url.toString();
+	        Image icon = new Image(str);
+	        primaryStage.getIcons().add(icon);
+			
+			primaryStage.show();
+			
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
