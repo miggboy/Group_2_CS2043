1e6f2ff78d35ddc7ec90122600868195fb0e5d97
Ben <109969208+DigiBT@users.noreply.github.com>
2023-11-16 14:55:01 -0400

    Add files via upload
    Added files for functionality of an add ingredient button.

--- Frontend/PrimaryController.java
+++ Frontend/PrimaryController.java
@@ -1,5 +1,3 @@
-package Frontend;
-
 import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
@@ -8,18 +6,27 @@ import java.util.ResourceBundle;
 import Backend.Ingredient;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.ListView;
+import javafx.stage.Stage;
 
 public class PrimaryController implements Initializable{
+
+    @FXML
+    private Button addNewIngredient;
 	
 	@FXML
 	private ListView<Ingredient> ingredientsListView;
 	
 	@FXML
 	private ListView<Ingredient> selectedListView;
-	
+
 	private ArrayList<Ingredient> ingredientArray = new ArrayList<Ingredient>();
 	private Ingredient currentIngredient;
 
@@ -48,5 +55,25 @@ public class PrimaryController implements Initializable{
 			
 		});
 	}
+
+    /** 
+     * This Action Event button loads a second scene onto the stage, prompting the user to enter a new ingredient.
+     * @param event
+     */
+    @FXML
+    void addIngredientAction(ActionEvent event) {
+        try {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("addIngredient.fxml"));
+            Parent root1 = loader.load();
+         
+            Stage stage1 = new Stage();
+            Scene scene1 = new Scene(root1);
+            stage1.setScene(scene1);
+            stage1.show();
+        } catch (IOException e) {
+            System.out.println("Cannot load new window: " + e.getMessage());
+        }
+    }
+
 
 }
