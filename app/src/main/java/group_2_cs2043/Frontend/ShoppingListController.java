787d0f816d2374584693f36873169f9a69a973d5
migg <migg@live.ca>
2023-11-23 07:53:47 -0400

    Fully implemented shopping list functionality.

--- app/src/main/java/group_2_cs2043/Frontend/ShoppingListController.java
+++ app/src/main/java/group_2_cs2043/Frontend/ShoppingListController.java
@@ -1,19 +1,27 @@
 package group_2_cs2043.Frontend;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 import group_2_cs2043.Backend.Recipe;
+import group_2_cs2043.Backend.RecipeIngredient;
 import group_2_cs2043.Backend.Runtime;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 
 public class ShoppingListController implements Initializable{
 	
@@ -23,6 +31,10 @@ public class ShoppingListController implements Initializable{
 	TableView<Recipe> selRecTable;
 	@FXML
 	TableColumn<Recipe, String> selRecColumn;
+	@FXML
+	TableView<RecipeIngredient> missIngTable;
+	@FXML
+	TableColumn<RecipeIngredient, String> missIngColumn;
 	
 	private Runtime runtime = new Runtime();
 	private ObservableList<String> recList;
@@ -42,8 +54,9 @@ public class ShoppingListController implements Initializable{
 		selRecColumn.setCellValueFactory(new PropertyValueFactory<Recipe, String>("name"));
 		selRecTable.setItems(selList);
 		
-		recipeBox.setOnAction(this::addRecipe);
+		missIngColumn.setCellValueFactory(new PropertyValueFactory<RecipeIngredient, String>("ingredientName"));
 		
+		recipeBox.setOnAction(this::addRecipe);
 	}
 	
 	/**
@@ -56,6 +69,31 @@ public class ShoppingListController implements Initializable{
 		selList.add(rec);
 	}
 	
+	/**
+	 * This method generates and displays a list of missing ingredients.
+	 */
+	@FXML
+	public void onGenerateClick() {
+		ArrayList<Recipe> recArrList = new ArrayList<Recipe>();
+		recArrList.addAll(selList);
+		ArrayList<RecipeIngredient> recIngList= runtime.makeShoppingList(recArrList);
+		ObservableList<RecipeIngredient> obsRecIngList = FXCollections.observableArrayList();
+		obsRecIngList.addAll(recIngList);
+		missIngTable.setItems(obsRecIngList);	
+	}
+	
+	/**
+	 * This method returns to the Recipe View scene.
+	 * @throws IOException 
+	 */
+	@FXML
+	public void onReturnClick(ActionEvent event) throws IOException {
+		Parent root = FXMLLoader.load(getClass().getResource("/recipeScreen.fxml"));
+	    Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+	    Scene scene = new Scene(root);
+	    stage.setScene(scene);
+	    stage.show();
+	}
 	
 	/**
 	 * Helper method for finding a Recipe's index based on name
@@ -69,10 +107,5 @@ public class ShoppingListController implements Initializable{
 			}
 		}
 		return index;
-	}
-	
-	
-	
-	
-	
+	}	
 }
